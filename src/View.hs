fbfb7eafb404b9ff7fbd005d1d3b1c1f087d743f
chl131 <chl131@ucsd.edu>
2021-12-01 15:33:00 -0800

    add imports Brick.Widgets.Border/Center

--- src/View.hs
+++ src/View.hs
@@ -1,4 +1,8 @@
 import qualified Brick.Widgets.Border.Style as BS
+import qualified Brick.Widgets.Border as B
+import qualified Brick.Widgets.Border.Style as BS
+import qualified Brick.Widgets.Center as C
+import qualified Graphics.Vty as V
 import Brick
   ( App(..), AttrMap, BrickEvent(..), EventM, Next, Widget
   , customMain, neverShowCursor
