e94a004262d32309d59c9a7c788b846943755ef1
cse230 <taipei.yuka@gmail.com>
2021-12-07 17:27:26 -0800

    fixed Result

--- src/Model.hs
+++ src/Model.hs
@@ -17,14 +17,14 @@ init = PS
   { racket1 = Player.player1                  
   , racket2 = Player.player2                     
   , ball    = Ball.init            
-  , result  = (Cont ())           
+  , result  = Nothing          
   , turn    = P1                    
   , score   = (0, 0)                
   }
 
-next :: PlayState -> Ball.Result Ball.Ball -> Either (Bool) PlayState
+next :: PlayState -> Ball.Result Ball.Ball -> Either (Maybe Turn) PlayState
 next s (Cont b') = Right (s { ball = b' } )
 next s (Hit pl) = Right (s { ball = Ball.reflect (ball s) pl })
 next s (Score p) = case (Score.addScore (score s) p) of
-                         Left winner -> Left winner
+                         Left winner -> Left (Just winner)
                          Right sc -> Right (s { ball = Ball.serveBall p, score = sc })
--- src/Model/Score.hs
+++ src/Model/Score.hs
@@ -3,9 +3,9 @@ module Model.Score where
 import Types
 --import Model.Ball
 
-addScore :: Score -> Turn -> Either (Bool) (Score) -- ^ Left winner, Right current Score after increment
+addScore :: Score -> Turn -> Either (Turn) (Score) -- ^ Left winner, Right current Score after increment
 addScore s t = let (s1,s2) = s in case t of
-                                        P1 -> if s1 +1 > 11 then Left True
+                                        P1 -> if s1 +1 > 11 then Left P1
                                               else Right (s1+1,s2)
-                                        P2 -> if s2 +1 > 11 then Left False
+                                        P2 -> if s2 +1 > 11 then Left P2
                                               else Right (s1,s2+1)
