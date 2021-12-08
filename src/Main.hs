f82023c96731ba7707a19db05c0e0f0fe62c0cb2
HeidiCheng <heidi8844@gmail.com>
2021-12-07 20:11:26 -0800

    Main change appDraw

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,7 @@ import Brick.BChan (newBChan, writeBChan)
 import Control.Monad (forever)
 import Control.Concurrent (threadDelay, forkIO)
 
+import Types
 import Model
 import View 
 import Control 
@@ -17,19 +18,18 @@ import Data.Maybe (fromMaybe)
 -------------------------------------------------------------------------------
 main :: IO ()
 main = do
-  rounds <- fromMaybe defaultRounds <$> getRounds
   chan   <- newBChan 10
   forkIO  $ forever $ do
     writeBChan chan Tick
     threadDelay 100000 -- decides how fast your game moves
   let buildVty = V.mkVty V.defaultConfig
   initialVty <- buildVty
-  res <- customMain initialVty buildVty (Just chan) app (Model.init rounds)
+  res <- customMain initialVty buildVty (Just chan) app Model.init
   print (result res, score res) 
 
-app :: App PlayState Tick String
+app :: App PlayState Tick Name
 app = App
-  { appDraw         = view 
+  { appDraw         = drawUI 
   , appChooseCursor = const . const Nothing
   , appHandleEvent  = control 
   , appStartEvent   = return
