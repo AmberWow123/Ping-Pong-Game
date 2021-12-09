355a3cfea21942566786e25da6286ecb17a76407
chl131 <chl131@ucsd.edu>
2021-12-09 15:45:39 -0800

    Add comments in View.hs

--- src/View.hs
+++ src/View.hs
@@ -11,15 +11,19 @@ import Brick(
   )
 import Model.Ball
 
+-- draw the game board and the score board, the score board is on the top of the game board
 drawUI :: PlayState -> [Widget Name]
 drawUI g = [C.center $ padTop (Pad 2) (drawStats g) <=> drawBoard g]
 
+-- draw the score board, it will also show whether a player wins a game
 drawStats :: PlayState -> Widget Name
 drawStats g = hLimit 20
   $ vBox [ drawScore (score g)
          , padTop (Pad 1) $ drawGameOver (result g)
          ]
 
+-- display the score of player1 and player2
+-- player1's score is on the left side and player2's socre is on the right side
 drawScore :: (Int, Int) -> Widget Name
 drawScore (p1_score, p2_score) = withBorderStyle BS.unicodeBold
   $ B.borderWithLabel (str "Score")
@@ -27,16 +31,18 @@ drawScore (p1_score, p2_score) = withBorderStyle BS.unicodeBold
   $ padAll 1
   $ str $ show (p1_score, p2_score)
 
+-- display the state that a player wins the game
 drawGameOver :: Maybe Turn -> Widget Name
 drawGameOver Nothing = emptyWidget
 drawGameOver (Just P1) = withAttr gameOverAttr $ C.hCenter $ str "PLAYER 1 WON"
 drawGameOver (Just P2) = withAttr gameOverAttr $ C.hCenter $ str "PLAYER 2 WON"
 
-gameOverAttr :: AttrName
-gameOverAttr = attrName "gameOver"
-
--- ==================================================================
-
+{-
+display the game board, the main idea is:
+1. make a board (grid) with the given height and width
+2. check if a coordinate (cell) of the board is a racket or ball
+3. painting each cell with its attribute
+-}
 drawBoard :: PlayState -> Widget Name
 drawBoard g = withBorderStyle BS.unicodeBold
   $ B.borderWithLabel (str "Game")
@@ -64,17 +70,21 @@ drawCell Racket     = withAttr racketAttr cw
 drawCell ViewBall   = withAttr ballAttr cb
 drawCell Empty      = withAttr emptyAttr cw
 
+-- output character of rackets and ball
 cw :: Widget Name
 cw = str " "
 
 cb :: Widget Name
 cb = str " "
 
-racketAttr, ballAttr, emptyAttr :: AttrName
+-- AttrName of each item
+gameOverAttr, racketAttr, ballAttr, emptyAttr :: AttrName
+gameOverAttr = attrName "gameOver"
 racketAttr = attrName "racketAttr"
 ballAttr   = attrName "ballAttr"
 emptyAttr  = attrName "emptyAttr"
 
+-- set the color of each item
 theMap :: AttrMap
 theMap = attrMap V.defAttr
   [ (racketAttr, V.blue `on` V.blue)
