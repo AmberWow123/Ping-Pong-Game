46f08a8dbb75a0705b61881f202ad46f290afd82
AmberWow123 <eatingwow@gmail.com>
2021-12-10 03:31:40 -0800

    added comments for Score.hs

--- src/Model/Score.hs
+++ src/Model/Score.hs
@@ -3,9 +3,13 @@ module Model.Score where
 import Types
 --import Model.Ball
 
-addScore :: Score -> Turn -> Either (Turn, Score) (Score) -- ^ Left winner, Right current Score after increment
+-- ^ Left winner, Right current Score after increment
+addScore :: Score -> Turn -> Either (Turn, Score) (Score)
 addScore s t = let (s1,s2) = s in case t of
-                                        P1 -> if s1 +1 >= 5 then Left (P1, (5, s2))
-                                              else Right (s1+1,s2)
-                                        P2 -> if s2 +1 >= 5 then Left (P2, (s1, 5))
-                                              else Right (s1,s2+1)
+      -- check if the score is larger than the max score
+      -- if so, set the winner
+      -- otherwise, continue the game
+      P1 -> if s1 +1 >= 5 then Left (P1, (5, s2))
+                        else Right (s1+1,s2)
+      P2 -> if s2 +1 >= 5 then Left (P2, (s1, 5))
+                        else Right (s1,s2+1)
